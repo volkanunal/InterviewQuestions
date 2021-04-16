8d997b157d80e80aed454cf90d8fa98a640110ed
Volkan <engineeringvolkan@hotmail.com>
2021-04-16 12:36:36 +0300

    solution without kmp

---
+++ Algorithm/KMP/withoutkmp.c
@@ -0,0 +1,72 @@
+#include "stdio.h"
+#include "string.h"
+#include "stdlib.h"
+
+int main()
+{
+    int ret  = 0;
+    
+    char* text = "bbabaa";
+   
+    char* temp_array = (char*)malloc(sizeof(char) * strlen(text));
+
+    memset(temp_array, 0, strlen(text) * sizeof(char));
+
+    for (int i = 0; i < strlen(text); ++i)
+    {
+        if (text[i] == 'b' && text[i + 1] == 'a')
+        {
+            temp_array[i] = 2;
+            temp_array[i + 1] = 0;
+            i++;
+        }
+
+        else if (text[i] == 'b' && text[i + 1] == 'b')
+        {
+            temp_array[i] = 1;
+            temp_array[i + 1] = 0;
+            i++;
+        }
+
+        else if(text[i] == 'a')
+        {
+            temp_array[i] = 0;
+        }
+                  
+    }
+
+    int is_any_a_flag = 0;
+
+    for (int i = 0; i < strlen(text); ++i)
+    {       
+        if (temp_array[i + 1] == 0 && temp_array[i] == 1)
+        {
+            ret += 2;
+            i++;
+        }
+
+        else if (temp_array[i + 1] == 2 && temp_array[i] == 1) //for bba
+        {
+            ret += 2;
+            i++;
+            is_any_a_flag = 1;
+        }
+
+        else if (temp_array[i] == 2 && temp_array[i + 1] == 0)
+        {
+            ret++;
+            i++;
+            is_any_a_flag = 1;
+        }         
+
+    }
+
+    printf("\r\n Ret %d",  is_any_a_flag ? ret : 0);
+
+    for (int i = 0; i < strlen(text); ++i)
+    {
+        printf("\r\n temp_array[i] : %i", temp_array[i]);
+    }
+
+    free(temp_array);
+}
